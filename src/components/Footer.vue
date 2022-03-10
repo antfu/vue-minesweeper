0522006aaaf58600647e2a6dbe32ff2811ea3120
Li Kui <90845831+likui628@users.noreply.github.com>
2022-03-10 10:43:20 +0800

    chore: set favicon & title & url (#5)

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -13,7 +13,7 @@ import { isDark, toggleDark } from '~/composables'
       class="icon-btn"
       i-carbon-logo-github
       rel="noreferrer"
-      href="https://github.com/antfu/vitesse-lite"
+      href="https://github.com/antfu/vue-minesweeper"
       target="_blank"
       title="GitHub"
     />
